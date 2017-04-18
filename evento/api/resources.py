3355ed15030bceaf543fa10b932534cee19c776b
Linguagem de Programação Comercial <lpc.pmw@labin.canoas>
2017-04-17 21:43:35 -0300

    Primeiro commit api

--- evento/api/resources.py
+++ evento/api/resources.py
@@ -1,5 +1,5 @@
 from tastypie.resources import ModelResource
-from evento.models import TipoInscricao
+from evento.models import *
 from django.contrib.auth.models import User
 
 
@@ -13,3 +13,8 @@ class UserResource(ModelResource):
     class Meta:
         queryset = User.objects.all()
         resource_name = 'user'
+
+class PessoaResource(ModelResource):
+    class Meta:
+        queryset = Pessoa.objects.all()
+        allowed_methods = ['get']
--- lpc_api/urls.py
+++ lpc_api/urls.py
@@ -16,13 +16,14 @@ Including another URLconf
 from django.conf.urls import url, include
 from tastypie.api import Api
 from django.contrib import admin
-from evento.api.resources import TipoInscricaoResource, UserResource
+from evento.api.resources import TipoInscricaoResource, UserResource,PessoaResource
 
 from evento.views import *
 
 v1_api = Api(api_name='v1')
 v1_api.register(TipoInscricaoResource())
 v1_api.register(UserResource())
+v1_api.register(PessoaResource())
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
